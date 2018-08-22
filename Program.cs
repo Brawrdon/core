6c995d1bb29a35de553e429da4ece428b3cf36e4
Brawrdon <BrandonOkeke@gmail.com>
2018-08-22 23:22:40 +0100

    Set up foundation for async HTTP listener

--- Program.cs
+++ Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.Net;
+using System.Threading;
 
-namespace Server
+namespace Brawrdon.Messenger
 {
     class Program
     {
+        private static HttpListener listener;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            listener = new HttpListener();
+            listener.Prefixes.Add("http://localhost:15001/");
+            listener.Start();
+            Console.WriteLine("Listening... Press enter to stop");
+
+            listener.BeginGetContext(ListenerCallback, null);
+            Console.ReadLine();
+        }
+
+        private static void ListenerCallback(IAsyncResult ar)
+        {
+            Random random = new Random();
+            HttpListenerContext context = listener.EndGetContext(ar);
+            listener.BeginGetContext(ListenerCallback, null);
+            Thread.Sleep(random.Next(1, 7) * 1000);
+            Console.WriteLine(context.Request.ContentLength64);
+            Console.WriteLine("Listening again..");
+
         }
     }
 }
