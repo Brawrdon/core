c2e8f54b216f9db4b43b34624ca8a02c1f77e907
Brawrdon <BrandonOkeke@gmail.com>
2018-08-23 22:20:17 +0100

    Testing different responses for specific ports

--- Server/Listener.cs
+++ Server/Listener.cs
@@ -1,32 +1,42 @@
 ﻿using System;
+using System.IO;
 using System.Net;
-using System.Threading;
 
 namespace Server
 {
-    class Listener
+    internal static class Listener
     {
-        private static HttpListener listener;
+        private static HttpListener _listener;
+        private const string twitter = "http://localhost:9801/";
 
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            listener = new HttpListener();
-            listener.Prefixes.Add("http://localhost:15001/");
-            listener.Start();
+            _listener = new HttpListener();
+            _listener.Prefixes.Add("http://localhost:15001/");
+            _listener.Prefixes.Add(twitter);
+
+            _listener.Start();
             Console.WriteLine("Listening... Press enter to stop");
 
-            listener.BeginGetContext(ListenerCallback, null);
+            _listener.BeginGetContext(ListenerCallback, null);
             Console.ReadLine();
         }
 
         private static void ListenerCallback(IAsyncResult ar)
         {
-            Random random = new Random();
-            HttpListenerContext context = listener.EndGetContext(ar);
-            listener.BeginGetContext(ListenerCallback, null);
-            Thread.Sleep(random.Next(1, 7) * 1000);
-            Console.WriteLine(context.Request.ContentLength64);
-            Console.WriteLine("Listening again..");
+            var context = _listener.EndGetContext(ar);
+            _listener.BeginGetContext(ListenerCallback, null);
+
+            if (context.Request.LocalEndPoint.Port == 9801)
+            {
+                Console.WriteLine("Tweet");
+            }
+            
+            using(var reader = new StreamReader(context.Request.InputStream))
+            {
+                Console.WriteLine(reader.ReadToEnd());
+
+            }
 
         }
     }
