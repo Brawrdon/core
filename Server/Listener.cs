17e7132f537db3aa520b953ec011e67c35a32424
Brawrdon <BrandonOkeke@gmail.com>
2018-08-26 22:27:10 +0100

    Made the server into more of an API

--- Server/Listener.cs
+++ Server/Listener.cs
@@ -7,13 +7,12 @@ namespace Dinosaur.Server
     internal static class Listener
     {
         private static HttpListener _listener;
-        private const string Twitter = "http://localhost:9801/";
 
         private static void Main(string[] args)
         {
+            // TODO: Add exceptions because this broke when I tried to ping
             _listener = new HttpListener();
             _listener.Prefixes.Add("http://localhost:15001/");
-            _listener.Prefixes.Add(Twitter);
 
             _listener.Start();
             Console.WriteLine("Listening... Press enter to stop");
@@ -24,17 +23,53 @@ namespace Dinosaur.Server
 
         private static void ListenerCallback(IAsyncResult ar)
         {
-            var context = _listener.EndGetContext(ar);
+            var request = _listener.EndGetContext(ar).Request;
+            var requestUrl = request.RawUrl.ToLower();
+
+            // Appends a backslash to the end of the request URL to make sure checks are done properly
+            if (!requestUrl.EndsWith("/"))
+            {
+                requestUrl += "/";
+            }
+
             _listener.BeginGetContext(ListenerCallback, null);
 
             // TODO: Send response back if statement not reached
-            if (context.Request.LocalEndPoint.Port == 9801 && context.Request.HttpMethod.ToUpper().Equals("POST"))
+            if (request.HttpMethod.ToUpper().Equals("POST"))
             {
-                using (var reader = new StreamReader(context.Request.InputStream))
+                // Parse Twitter requests
+                if (requestUrl.StartsWith("/twitter"))
                 {
-                    Bots.Twitter.PostTweet(reader.ReadToEnd());
+                    if (request.ContentType.Equals("application/json"))
+                    {
+                        ProcessTwitterRequest(request, requestUrl);
+                    }
                 }
             }
+
+            GenerateResponse();
         }
+
+        private static void ProcessTwitterRequest(HttpListenerRequest request, string requestUrl)
+        {
+            // Removes /twitter from the url request to easily check what kind of request this is
+            requestUrl = requestUrl.Remove(0, 9);
+            
+            // Request to post a tweet
+            if (requestUrl.Equals("post/"))
+            {                        
+                // TODO: Check if the Json contains the message parameter, ignore everything else
+                using (var reader = new StreamReader(request.InputStream))
+                {
+                    Bots.Twitter.PostTweet(reader.ReadToEnd());
+                }               
+            }
+        }
+        
+        private static void GenerateResponse()
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
