9f16b6529568fd48af7bcb88cc7c7d98c35128d4
Brawrdon <BrandonOkeke@gmail.com>
2018-10-09 12:15:38 +0100

    Restructure project to focus on BrawrdonBot

---
+++ BrawrdonBot/Api.cs
@@ -0,0 +1,10 @@
+﻿namespace BrawrdonBot
+{
+    public static class Api
+    {
+        public const string ConsumerKey = "";
+        public const string ConsumerKeySecret = "";
+        public const string OauthToken = "";
+        public const string OauthTokenSecret = "";
+    }
+}
